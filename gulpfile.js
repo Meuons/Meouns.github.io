d91cc58c7c9bb1846c40189214a1037e947a029d
Meuons <89832844+Meuons@users.noreply.github.com>
2021-09-09 12:57:24 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const {src, dest, watch, series, parallel} = require("gulp");
 const concat = require('gulp-concat');
-const uglify = require ('gulp-uglify-es').default;
 const minifyCSS = require('gulp-minify-css');
+const terser = require('gulp-terser');
 const livereload = require('gulp-livereload');
 const options = {
     html: {
@@ -11,44 +11,50 @@ const options = {
 };
 
 livereload({ start: true })
-//Sökvägar 
+//Filepaths
 const files = { 
     htmlPath: "src/**/*.html",
     cssPath: "src/css/*.css",
-    jsPath: "src/**/*.js"
+    jsPath: "src/**/*.js",
+    imgPath: "src/**/*.jpg"
 }
 
-//HTML-task, kopiera filer
-function copyHTML() {
+//HTML-task, duplicate files
+function htmlTask() {
 return src(files.htmlPath)
 
 .pipe(dest('pub'))
 
 }
-
-
-//CSS-task, konkatinera, minifiera
+//Img-task, duplicate files
+function imgTask() {
+    return src(files.imgPath)
+    
+    .pipe(dest('pub'))
+    
+    }
+//CSS-task, minify, concat
 function cssTask() {
     return src(files.cssPath)
-    .pipe(concat('main.css'))
+    .pipe(concat('styles.css'))
     .pipe(dest('pub/css'))
     .pipe(minifyCSS())
     
 }
-//JS-task, konkatinera, minifiera
+//JS-task,  minify, concat
 function jsTask() {
     return src(files.jsPath)
     .pipe(concat('scripts.js'))
-    .pipe(uglify())
+    .pipe(terser())
     .pipe(dest('pub/js'));
 }
 //Watch
 function watchTask(){
-  watch([files.htmlPath, files.jsPath, files.cssPath], parallel(copyHTML, jsTask, cssTask));
+  watch([files.htmlPath, files.jsPath, files.cssPath, files.imgPath], parallel(htmlTask, jsTask, cssTask, imgTask));
 }
 
 
 exports.default = series(
-    parallel(copyHTML, jsTask, cssTask),
+    parallel(htmlTask, jsTask, cssTask, imgTask),
     watchTask
 );
