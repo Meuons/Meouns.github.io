6024398e22e186ed34f272f8266867dac96aee6a
Meuons <89832844+Meuons@users.noreply.github.com>
2021-09-24 11:56:12 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const {src, dest, watch, series, parallel} = require("gulp");
 const concat = require('gulp-concat');
 const minifyCSS = require('gulp-minify-css');
 const terser = require('gulp-terser');
-const livereload = require('gulp-livereload');
+var browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+
 
 const options = {
     html: {
@@ -11,54 +13,56 @@ const options = {
     },
 };
 
+const gulp = require('gulp');
 
 
-
-livereload({ start: true })
 //Filepaths
 const files = { 
     htmlPath: "src/**/*.html",
-    cssPath: "src/css/*.css",
     jsPath: "src/**/*.js",
-    imgPath: "src/**/*.jpg"
+    imgPath: "src/**/*.jpg",
+    cssPath: "src/**/*.css"
 }
 
+function cssTask() {
+    return src(files.cssPath)
+    .pipe(concat('styles.css'))
+        .pipe(minifyCSS())
+        .pipe(dest("pub/css"))
+        .pipe(browserSync.stream());
+}
+        
 //HTML-task, duplicate files
 function htmlTask() {
 return src(files.htmlPath)
-
 .pipe(dest('pub'))
+.pipe(browserSync.stream());
 
 }
 //Img-task, duplicate files
 function imgTask() {
     return src(files.imgPath)
-    
     .pipe(dest('pub'))
+    .pipe(browserSync.stream());
     }
-//CSS-task, minify, concat
-function cssTask() {
-    return src(files.cssPath)
-    .pipe(concat('styles.css'))
-    .pipe(minifyCSS())
-    .pipe(dest('pub/css'))
-    .pipe(livereload());
-}
+
 //JS-task,  minify, concat
 function jsTask() {
     return src(files.jsPath)
     .pipe(concat('scripts.js'))
     .pipe(terser())
     .pipe(dest('pub/js'))
-    .pipe(livereload());
+    .pipe(browserSync.stream());
 }
+
 //Watch
+
 function watchTask(){
-    livereload.listen();
-  watch([files.htmlPath, files.jsPath, files.cssPath, files.imgPath], parallel(htmlTask, jsTask, cssTask, imgTask)).on('change', livereload.changed);
-}
-
-
+    browserSync.init({
+      server: "./pub"
+    });
+    watch([files.htmlPath, files.jsPath, files.imgPath, files.cssPath], parallel(htmlTask, jsTask, imgTask, cssTask)).on("change", browserSync.reload);
+  }
 exports.default = series (
     parallel(htmlTask, jsTask, imgTask, cssTask),
     watchTask
